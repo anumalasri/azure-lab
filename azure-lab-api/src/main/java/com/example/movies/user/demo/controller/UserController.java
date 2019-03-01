7df4539e95cdd9ad3751557f10f61d7f1b09cfe6
Srinivas Anumala <srinivas.anumala@metlife.com>
2019-03-01 10:10:28 -0500

    Update user movies

--- azure-lab-api/src/main/java/com/example/movies/user/demo/controller/UserController.java
+++ azure-lab-api/src/main/java/com/example/movies/user/demo/controller/UserController.java
@@ -3,9 +3,15 @@ package com.example.movies.user.demo.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.bson.Document;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,7 +21,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.movies.user.demo.domain.Movie;
 import com.example.movies.user.demo.domain.User;
+import com.example.movies.user.repository.MovieRepository;
 import com.example.movies.user.repository.UserRepository;
 
 
@@ -27,6 +35,15 @@ public class UserController {
 	
 	@Autowired
 	private UserRepository repository;
+	
+	@Autowired
+	private MovieRepository movieRepo;
+	
+	
+	
+	@Autowired
+	private MongoTemplate template;
+	
 
 	@GetMapping()
 	List<User> getUser() {
@@ -75,4 +92,42 @@ public class UserController {
 		
 	}
 
+	
+	@GetMapping("/{username}/updateMovies")
+	public String updateUserMovies(@PathVariable String username) {
+		User user=repository.findByUsername(username);
+		
+		List<String> preferneces = user.getPreferences();
+		
+		Query query = new Query();
+		
+		query.addCriteria(Criteria.where("genres").in(preferneces));
+		query.with(new Sort(Sort.Direction.DESC, "$imdb.rating"));
+		query.fields().include("title").include("year").include("imdb");
+		query.with(PageRequest.of(0, 20));
+		
+		List<Document> movies = template.find(query, Document.class, "movies");
+		
+		List<Object> movies2 = new ArrayList<Object>();
+		
+		movies.forEach(mov-> {
+			Document doc = new Document();
+			doc.put("id", mov.get("_id").toString());
+			doc.put("title", mov.getString("title"));
+			doc.put("year", mov.getInteger("year"));
+			Document imdb = (Document) mov.get("imdb");
+			doc.put("rating", imdb.get("rating"));
+			movies2.add(doc);
+		});
+		
+		 
+		
+		//List<Object> movies = movieRepo.finduserPreferredMovies(preferneces, PageRequest.of(1, 20));
+		
+		user.setMovies(movies2);
+		repository.save(user);
+		
+		return "Successfully Updated User Preferred Movies";
+	}
+	
 }
--- azure-lab-api/src/main/java/com/example/movies/user/repository/MovieRepository.java
+++ azure-lab-api/src/main/java/com/example/movies/user/repository/MovieRepository.java
@@ -2,7 +2,9 @@ package com.example.movies.user.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.movies.user.demo.domain.Movie;
@@ -15,7 +17,8 @@ public interface MovieRepository extends MongoRepository <Movie, String> {
 	
 	Movie findByTitle(String title);
 
- 
+	@Query(value="{ 'genres': { '$in' : [?0] } }", sort = "{ 'imdb.rating': -1 }", fields="{ 'title' : 1, 'year' : 1, 'imdb.rating' : 1}")
+	List<Object> finduserPreferredMovies(List<String> genres, Pageable page);
 	
 //	List<Object> findByMoviePreference(String [] moviePreference);
 }
